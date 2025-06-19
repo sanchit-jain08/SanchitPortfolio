b0503fa3d3b967c849e718bddb84dcbbcbd0f92c
sanchit-jain08 <149262985+sanchit-jain08@users.noreply.github.com>
2025-06-19 23:13:06 +0530

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -35,7 +35,7 @@ with open(resume_file, "rb") as pdf_file:
 profile_pic = Image.open(profile_pic)
 
 # --- HERO SECTION ---
-col1, col2 = st.columns(3, gap="small")
+col1, col2, col3 = st.columns(3, gap="small")
 with col1:
     st.image(profile_pic, width=230)
 with col2:
